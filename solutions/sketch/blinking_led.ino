a94fa39122afff2423186787ef61f26d180391d3
Aniket Dinda <dinda.aniket@gmail.com>
2026-09-03 23:55:45 +0530

    Update blinking_led.ino

--- solutions/sketch/blinking_led.ino
+++ solutions/sketch/blinking_led.ino
@@ -1,39 +1,12 @@
-// 7-segment display
-// Common cathode
-// Segments: a, b, c, d, e, f, g
-
-int segments[] = {2, 3, 4, 5, 6, 7, 8};
-
-// Each row represents a digit 0-9
-// Order: a b c d e f g
-byte digits[10][7] = {
-  {1, 1, 1, 1, 1, 1, 0}, // 0
-  {0, 1, 1, 0, 0, 0, 0}, // 1
-  {1, 1, 0, 1, 1, 0, 1}, // 2
-  {1, 1, 1, 1, 0, 0, 1}, // 3
-  {0, 1, 1, 0, 0, 1, 1}, // 4
-  {1, 0, 1, 1, 0, 1, 1}, // 5
-  {1, 0, 1, 1, 1, 1, 1}, // 6
-  {1, 1, 1, 0, 0, 0, 0}, // 7
-  {1, 1, 1, 1, 1, 1, 1}, // 8
-  {1, 1, 1, 1, 0, 1, 1}  // 9
-};
-
 void setup() {
-  for (int i = 0; i < 7; i++) {
-    pinMode(segments[i], OUTPUT);
-  }
+  pinMode(13, OUTPUT);
 }
 
-void displayDigit(int number) {
-  for (int i = 0; i < 7; i++) {
-    digitalWrite(segments[i], digits[number][i]);
-  }
-}
+void loop(){
+  digitalWrite(13,HIGH) ;
+  delay(1000) ;
 
-void loop() {
-  for (int number = 0; number <= 9; number++) {
-    displayDigit(number);
-    delay(1000);
-  }
+  digitalWrite(13,LOW) ;
+  delay (1000) ;
+  
 }
